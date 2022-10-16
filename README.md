d0d62d41f04bbde07afe140442d64942dedf5012
Siddharth Singh <iamsidz89@gmail.com>
2022-10-16 23:00:07 +0530

    auth app completed using passport and mongodb

---
+++ README.md
@@ -0,0 +1,5 @@
+# Node.js & Passport Login
+
+This is a user login and registration app using Node.js, Express, Passport, Mongoose, EJS and some other packages.
+
+This is my codin ninjas node js assessment 1.
---
+++ config/keys.js
@@ -0,0 +1,8 @@
+dbPassword = 'mongodb+srv://siddharth:123anurag@authentication.lbodm9u.mongodb.net/?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
+
+//  we are using mongo db atlas for data storage 
